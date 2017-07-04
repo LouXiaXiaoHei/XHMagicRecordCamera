7210248dc49166a5beb6a46067479f09930c26ff
邓小涛 <1319697214@qq.com>
2017-07-04 17:25:23 +0800

    Init

---
+++ Example/Pods/Target Support Files/Pods-XHMagicRecordCamera_Tests/Pods-XHMagicRecordCamera_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XHMagicRecordCamera_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XHMagicRecordCamera_Tests
+@end
---
+++ Example/Pods/Target Support Files/XHMagicRecordCamera/XHMagicRecordCamera-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XHMagicRecordCamera : NSObject
+@end
+@implementation PodsDummy_XHMagicRecordCamera
+@end
